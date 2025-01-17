8ffc9a6a32a97c22017c20a8a6db8789ce4c0355
Gevorg Hovhannisyan <gev9911@gmail.com>
2025-01-17 22:08:44 +0400

    [42Yerevan:patch] [gehovhan] - Update Nginx configuration.

--- srcs/requirements/nginx/tools/nginx_setup.sh
+++ srcs/requirements/nginx/tools/nginx_setup.sh
@@ -6,9 +6,14 @@ location="Yerevan"
 organization="42Yerevan"
 organization_unit="student"
 common_name="gehovhan.42.fr"
+ssl_key_path="/etc/nginx/ssl/gehovhan.42.fr.key"
+ssl_cert_path="/etc/nginx/ssl/gehovhan.42.fr.crt"
+
+# Ensure the directory exists
+mkdir -p /etc/nginx/ssl
 
 # Generate SSL certificate and key for NGINX
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SSL_KEY_PATH" -out "$SSL_CERT_PATH" -subj "/C=$country/L=$location/O=$organization/OU=$organization_unit/CN=$common_name"
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout "$ssl_key_path" -out "$ssl_cert_path" -subj "/C=$country/L=$location/O=$organization/OU=$organization_unit/CN=$common_name"
 
 cd /etc/nginx/sites-available/ || exit 1
 
